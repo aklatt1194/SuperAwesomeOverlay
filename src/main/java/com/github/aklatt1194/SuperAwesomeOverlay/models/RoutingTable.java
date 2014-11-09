a57af621bb5fda515bf0bae56f05b4ffc8eb01a6
Sidney Berg <sid@desktop>
2014-11-09 00:05:26 -0800

    fixed node hostnames

--- src/main/java/com/github/aklatt1194/SuperAwesomeOverlay/models/RoutingTable.java
+++ src/main/java/com/github/aklatt1194/SuperAwesomeOverlay/models/RoutingTable.java
@@ -2,10 +2,10 @@ package com.github.aklatt1194.SuperAwesomeOverlay.models;
 
 public class RoutingTable {
     public static final String[] NODES = {
-        "sidney.duckdns.org",
-        "ec2-54-171-49-149.eu-west-1.compute.amazonaws.com",
-        "ec2-54-65-17-109.ap-northeast-1.compute.amazonaws.com",
-        "ec2-54-173-133-146.compute-1.amazonaws.com" };
+        "sidney.duckdns.org",  // for testing purposes
+        "ec2-54-72-49-50.eu-west-1.compute.amazonaws.com",
+        "ec2-54-64-177-145.ap-northeast-1.compute.amazonaws.com",
+        "ec2-54-172-69-181.compute-1.amazonaws.com"};
     
     public String[] getKnownNodes() {
         return NODES;
