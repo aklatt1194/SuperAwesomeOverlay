b3015e82181c2b0c00732c340bab0ea8178fdaa2
Sidney Berg <sidneysea@gmail.com>
2014-11-29 18:44:49 -0800

    added three new nodes to scripts/mssh.sh

--- scripts/mssh.sh
+++ scripts/mssh.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-mssh ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com ubuntu@ec2-54-72-49-50.eu-west-1.compute.amazonaws.com ubuntu@ec2-54-64-177-145.ap-northeast-1.compute.amazonaws.com
+mssh \
+ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com \
+ubuntu@ec2-54-72-49-50.eu-west-1.compute.amazonaws.com \
+ubuntu@ec2-54-64-177-145.ap-northeast-1.compute.amazonaws.com \
+ubuntu@ec2-54-149-47-168.us-west-2.compute.amazonaws.com \
+ubuntu@ec2-54-93-174-218.eu-central-1.compute.amazonaws.com \
+ubuntu@ec2-54-66-216-87.ap-southeast-2.compute.amazonaws.com \
