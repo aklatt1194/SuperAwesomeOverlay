aeb9d6fd443e7baf0b5dc777643a00a58f2e409c
Sidney Berg <sidneysea@gmail.com>
2014-12-07 15:00:51 -0800

    switched out Ireland node

--- scripts/mssh.sh
+++ scripts/mssh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mssh \
-ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com \
-ubuntu@ec2-54-72-49-50.eu-west-1.compute.amazonaws.com \
+    ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com \
+    ubuntu@ec2-54-77-160-172.eu-west-1.compute.amazonaws.com \
 ubuntu@ec2-54-64-177-145.ap-northeast-1.compute.amazonaws.com \
 ubuntu@ec2-54-149-47-168.us-west-2.compute.amazonaws.com \
 ubuntu@ec2-54-93-174-218.eu-central-1.compute.amazonaws.com \
