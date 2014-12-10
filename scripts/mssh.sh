8d2635f24b377db44739fcc1b10c06e3cd6351ec
Sidney Berg <sidneysea@gmail.com>
2014-12-09 16:43:28 -0800

    added singapore

--- scripts/mssh.sh
+++ scripts/mssh.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 mssh \
-    ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com \
-    ubuntu@ec2-54-77-160-172.eu-west-1.compute.amazonaws.com \
+ubuntu@ec2-54-172-69-181.compute-1.amazonaws.com \
+ubuntu@ec2-54-77-160-172.eu-west-1.compute.amazonaws.com \
 ubuntu@ec2-54-64-177-145.ap-northeast-1.compute.amazonaws.com \
 ubuntu@ec2-54-149-47-168.us-west-2.compute.amazonaws.com \
 ubuntu@ec2-54-93-174-218.eu-central-1.compute.amazonaws.com \
 ubuntu@ec2-54-66-216-87.ap-southeast-2.compute.amazonaws.com \
 ubuntu@ec2-54-67-79-233.us-west-1.compute.amazonaws.com \
-ubuntu@ec2-54-94-157-221.sa-east-1.compute.amazonaws.com
+ubuntu@ec2-54-94-157-221.sa-east-1.compute.amazonaws.com \
+ubuntu@ec2-54-169-194-118.ap-southeast-1.compute.amazonaws.com
