7462b9f8fc8a0687afd73895cc6d60d84d004e94
Sidney Berg <sidneysea@gmail.com>
2014-12-09 20:05:56 -0800

    iperf script

---
+++ scripts/iperf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+iperf -s -p 4545 -D -o scripts/iperf.log
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-killall iperf
-iperf -s -p 4545 -D > scripts/iperf.log
 kill -9 `cat scripts/overlay.pid`
 nohup mvn exec:java -Dexec.mainClass="com.github.aklatt1194.SuperAwesomeOverlay.Overlay" > scripts/output.log &
 echo $! > scripts/overlay.pid
